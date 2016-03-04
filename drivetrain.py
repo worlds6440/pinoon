edd4720718d3a929b068fc966d724fd62a64a415
David <worlds6440@aol.com>
2016-03-04 20:29:01 +0000

    Correct omni mixer

--- drivetrain.py
+++ drivetrain.py
@@ -22,13 +22,13 @@ class DriveTrain():
         debug=False
     ):
         # Main set of motor controller ranges
-        self.servo_min = 900
-        self.servo_mid = 1650
-        self.servo_max = 2300
+        self.servo_min = 1000
+        self.servo_mid = 1500
+        self.servo_max = 2000
 
         # Full speed range
-        self.servo_full_min = 900
-        self.servo_full_max = 2300
+        self.servo_full_min = 1000
+        self.servo_full_max = 2000
         # Low speed range is 1/4 of full speed
         speed_divisor = 2
         self.servo_low_min = int(self.servo_mid - (self.servo_mid-self.servo_full_min) / speed_divisor)
@@ -117,15 +117,15 @@ class DriveTrain():
         self.set_servo_pulse(self.channels['left'], output_pulse_left)
         self.set_servo_pulse(self.channels['right'], output_pulse_right)
 
-    def mix_channels_omni_and_assign(self, throttle, steering, rotate):
+    def mix_channels_omni_and_assign(self, forward, side, rotate):
         """ Take values for throttle, steering and rotation channels
         in the range of -1 to 1, convert to servo pulses, and then mix
         the channels and assign to the left, right and front motors. """
         if not self.drive_enabled:
             return
-        pulse_throttle = self._map_channel_value(throttle)
-        pulse_steering = self._map_channel_value(steering)
-        pulse_rotate = self._map_channel_value(rotate)
+        pulse_forward = self._map_channel_value(forward) - self.servo_mid
+        pulse_side = self._map_channel_value(side) - self.servo_mid
+        pulse_rotate = self._map_channel_value(rotate) - self.servo_mid
 
         # Appears to be RC signal in, we don't need this section
         # VFwd = pulseIn(pin[0], HIGH)-1500
@@ -133,29 +133,31 @@ class DriveTrain():
         # VSide =pulseIn(pin[2], HIGH)-1500
         round_dp = 0
 
+        clip_value = (self.servo_max - self.servo_min) / 2.0
+
         # VFront = constrain(-VSide-VRotate, -500, 500)+1500
         output_pulse_front = clip(
-            (-pulse_steering-pulse_rotate),
-            self.servo_min,
-            self.servo_max
+            (-pulse_side-pulse_rotate),
+            -clip_value,
+            +clip_value
         )
         # VLeft = constrain(-round(VSide*0.15+VFwd*0.86-VRotate),-500,500)+1500
         output_pulse_left = clip(
             -round(
-                   pulse_steering*0.15+pulse_throttle*0.86-pulse_rotate,
+                   pulse_side*0.15+pulse_forward*0.86-pulse_rotate,
                    round_dp
             ),
-            self.servo_min,
-            self.servo_max
+            -clip_value,
+            +clip_value
         )
         # VRight = constrain(round(VSide*0.15-VFwd*0.86-VRotate),-500,500)+1500
         output_pulse_right = clip(
             round(
-                  pulse_steering*0.15-pulse_throttle*0.86-pulse_rotate,
+                  pulse_side*0.15-pulse_forward*0.86-pulse_rotate,
                   round_dp
             ),
-            self.servo_min,
-            self.servo_max
+            -clip_value,
+            +clip_value
         )
 
         # where pulsein recieves a ~1000-2000 pulse,
@@ -163,9 +165,12 @@ class DriveTrain():
         # 2000 = full right/forward. so Vfwd is +/-500, 0 = stopped.
 
         # Set the servo pulses for left and right channels
-        self.set_servo_pulse(self.channels['left'], output_pulse_left)
-        self.set_servo_pulse(self.channels['right'], output_pulse_right)
-        self.set_servo_pulse(self.channels['front'], output_pulse_front)
+        self.set_servo_pulse(self.channels['left'],
+                             output_pulse_left + self.servo_mid)
+        self.set_servo_pulse(self.channels['right'],
+                             output_pulse_right + self.servo_mid)
+        self.set_servo_pulse(self.channels['front'],
+                             output_pulse_front + self.servo_mid)
 
     def _map_channel_value(self, value):
         """Map the supplied value from the range -1 to 1 to a corresponding
