de3da8df8c4a7ee21b5cbf675cb26ace02fb2509
David <worlds6440@aol.com>
2016-03-22 20:36:21 +0000

    Testing button handler

--- ps3.py
+++ ps3.py
@@ -1,10 +1,6 @@
 from triangula.input import SixAxis, SixAxisResource
 
 
-# Button handler, will be bound to the square button later
-def handler(button):
-    print('Button {} pressed'.format(button))
-
 # Keep trying to connect the PS3 Controller
 while True:
     try:
@@ -13,15 +9,19 @@ while True:
         # that we should bind actions to the SELECT and START buttons to
         # centre the controller and reset the calibration respectively.
         with SixAxisResource(bind_defaults=True) as joystick:
-            # Register a button handler for the square button
-            joystick.register_button_handler(handler, SixAxis.BUTTON_SQUARE)
             while joystick.is_connected():
+
+                # Look for button presses
+                buttons_pressed = joystick.get_and_clear_button_press_history()
+                if buttons_pressed & 1 << SixAxis.BUTTON_SQUARE:
+                    print("Square button pressed")
+
                 # Read the x and y axes of the left hand stick,
                 # the right hand stick has axes 2 and 3
                 left_x = joystick.axes[0].corrected_value()
                 left_y = joystick.axes[1].corrected_value()
                 right_x = joystick.axes[2].corrected_value()
                 right_y = joystick.axes[3].corrected_value()
-                print("[" + str(left_x) + "," + str(left_y) + "]            [" + str(right_x) + "," + str(right_y) + "]")
+                print "[{:10.2f}, {:10.2f}][{:10.2f}, {:10.2f}]".format(left_x, left_y, right_x, right_y)
     except(IOError):
         print("Controller Not Connected")
